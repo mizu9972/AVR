2b1642ca5f659e8dc836cd57ec4bd2087caaad0f
hyuga0305 <csofumry@gmail.com>
2020-08-25 17:51:01 +0900

    タイトルシーンにゲームシーンにつなげるボタンと終了するボタンの作成

---
+++ ProjectAVR/Assets/Script/Title/TitleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] private string GameSceneName;
+
+    void Start()
+    {
+        
+    }
+
+    public void GoGameScene() {
+        SceneManager.LoadScene(GameSceneName);
+    }
+
+    public void Exit() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+}
