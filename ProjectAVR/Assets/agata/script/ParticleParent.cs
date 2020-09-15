8c1ed5d539518dbc0a940a89b0f3d7a2821b4f3e
mizu9972 <oisiimizu9972@icloud.com>
2020-09-15 18:31:31 +0900

    パーティクル発射・パーティクル生存管理スクリプト追加 弾パーティクル管理プレハブ作成

---
+++ ProjectAVR/Assets/agata/script/ParticleParent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleParent : MonoBehaviour
+{
+    [SerializeField, Header("パーティクルシステムオブジェクト")]
+    GameObject ParticleSystem = null;
+
+   // ParticleSystem m_BulletParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //m_BulletParticle = ParticleSystem.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Shot")]
+    public void Shot()
+    {
+        if (ParticleSystem != null)
+        {
+            Instantiate(ParticleSystem, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ ProjectAVR/Assets/agata/script/ParticleSelfDelete.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSelfDelete : MonoBehaviour
+{
+    ParticleSystem m_ParticleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ParticleSystem = this.gameObject.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_ParticleSystem.isPlaying == false)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
