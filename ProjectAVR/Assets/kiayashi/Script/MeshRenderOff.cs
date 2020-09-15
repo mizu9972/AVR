d336a20e39921724421d0e73ec4d294c33116d7c
syu0417 <s_kiba84@yahoo.co.jp>
2020-09-15 16:32:06 +0900

    トロッコ移動用の柱のプレハブを作成。ゲームスタート時に柱の描画をオフに

---
+++ ProjectAVR/Assets/kiayashi/Script/MeshRenderOff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshRenderOff : MonoBehaviour
+{
+    MeshRenderer MyRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRenderer = this.GetComponent<MeshRenderer>();
+        MyRenderer.enabled = false;
+    }
+}
