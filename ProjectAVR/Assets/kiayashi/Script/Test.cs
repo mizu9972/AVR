f9df26a7e4a80340e35edc457e59c85a7d907894
syu0417 <s_kiba84@yahoo.co.jp>
2020-09-17 15:51:37 +0900

    ケースで使用していたオーディオマネージャ実装

---
+++ ProjectAVR/Assets/kiayashi/Script/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.UpdateAsObservable().Take(1).
+            Subscribe(_ => AudioManager.Instance.PlayMainBGM("BGM_GAMEMAIN", true));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
