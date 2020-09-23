f085a70290d1007636a8277511f060907f6bd0a5
mizu9972 <oisiimizu9972@icloud.com>
2020-09-23 12:43:50 +0900

    弾を水弾に変更 水弾に当たり判定追加(処理は未実装)

---
+++ ProjectAVR/Assets/agata/script/ParticleBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //当たり判定処理
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Hit");
+    }
+}
