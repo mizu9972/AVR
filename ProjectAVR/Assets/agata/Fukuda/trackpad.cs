507168c88cb1b41fc030fbacb9155d10da7b6598
mizu9972 <oisiimizu9972@icloud.com>
2020-10-27 18:25:29 +0900

    雰囲気シーンに部屋追加 VRカメラをプレハブ化

--- ProjectAVR/Assets/agata/Fukuda/trackpad.cs
+++ ProjectAVR/Assets/agata/Fukuda/trackpad.cs
@@ -9,6 +9,8 @@ public class trackpad : MonoBehaviour
     private SteamVR_Action_Vector2 TrackPad = SteamVR_Actions.default_TrackPad;
 
     private Vector2 pos;
+
+    float r, sita;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,13 @@ public class trackpad : MonoBehaviour
     void Update()
     {
         pos = TrackPad.GetLastAxis(SteamVR_Input_Sources.RightHand);
-        transform.localPosition = new Vector3(pos.x * 0.1f, 0, pos.y * 0.1f);
+        transform.localPosition = new Vector3(pos.x, 0, pos.y);
+
+        r = Mathf.Sqrt(pos.x * pos.x + pos.y * pos.x);
+        sita = Mathf.Atan2(pos.y, pos.x) / Mathf.PI * 180;
+
+
+        Debug.Log(r + "" + sita);
     }
+
 }
