79ded2390a2d9289948324102632f2282fb2ea71
mizu9972 <oisiimizu9972@icloud.com>
2020-11-20 14:21:03 +0900

    アニメーション管理スクリプト追加

---
+++ ProjectAVR/Assets/agata/script/animation/EscapeAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeAnim : MonoBehaviour
+{
+    [SerializeField, Header("")]
+    private Animator animator = null;
+
+    [ContextMenu("アニメーション開始")]
+    public void StartAnim()
+    {
+        if(animator == null)
+        {
+            return;
+        }
+
+        animator.Play("Entry");
+    }
+}
