d9d5cbe70619d9b644127d012c993e4d52b98b2c
jyura <life_jyura@yahoo.co.jp>
2020-11-24 15:58:19 +0900

    モデル追加

--- ProjectAVR/Assets/tatemoto/script/prologscript.cs
+++ ProjectAVR/Assets/tatemoto/script/prologscript.cs
@@ -8,7 +8,8 @@ public class prologscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Meshrender meshrender = cap.GetComponent<MeshRenderer>();
+        meshrender.material.color = new Color(0, 0, 0, 1.0f);
     }
 
     // Update is called once per frame
@@ -17,6 +18,7 @@ public class prologscript : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             cap.transform.rotation = Quaternion.Euler(45, 0, 0);
+            meshrender.material.color = new Color(0, 0, 0, 0.0f);
         }
     }
 }
