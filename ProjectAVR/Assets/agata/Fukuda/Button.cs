3dced70f7c54f18b574e199255aea47001c72e1a
takenakahan <61533682+takenakahan@users.noreply.github.com>
2020-11-25 11:46:10 +0900

    おはようございますタイトル(仮)

---
+++ ProjectAVR/Assets/agata/Fukuda/Button.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR;
+using UnityEngine.UI;
+public class Button : MonoBehaviour
+{
+    private SteamVR_Action_Boolean actionToHaptic = SteamVR_Actions._default.TitleCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("HunikiDevelop");
+       
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.name == "Laser")
+        {
+            this.gameObject.GetComponent<Image>().color = Color.red;
+
+            if (actionToHaptic.GetStateDown(SteamVR_Input_Sources.RightHand))
+            {
+                OnClick();
+            }
+            
+        }
+        Debug.Log("clicked");
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        this.gameObject.GetComponent<Image>().color = Color.white;
+    }
+}
