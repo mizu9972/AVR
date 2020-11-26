1436485eb1c6962ed5a9104589b4f14089c9a46b
jyura <life_jyura@yahoo.co.jp>
2020-11-27 07:52:28 +0900

    プロローグ追加

---
+++ ProjectAVR/Assets/tatemoto/script/prologcamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prologcamera : MonoBehaviour
+{
+    Camera targetCamera;
+
+    [SerializeField]
+    Transform targetObj;
+    GameObject houki;
+
+    Rect rect = new Rect(0.3f, 0.3f, 0.7f, 0.7f);
+
+
+    void Start()
+    {
+        targetCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        var viewportPos = targetCamera.WorldToViewportPoint(targetObj.position);
+
+        if (rect.Contains(viewportPos))
+        {
+            FlagManager.Instance.flags[0] = true;
+        }
+        else
+        {
+
+        }
+
+    }
+}
--- ProjectAVR/Assets/tatemoto/script/prologscript.cs
+++ ProjectAVR/Assets/tatemoto/script/prologscript.cs
@@ -5,20 +5,49 @@ using UnityEngine;
 public class prologscript : MonoBehaviour
 {
     public GameObject cap;
+    public GameObject yogo;
+    public GameObject moveing;
+    public bool endflag = false;
     // Start is called before the first frame update
     void Start()
     {
-        Meshrender meshrender = cap.GetComponent<MeshRenderer>();
-        meshrender.material.color = new Color(0, 0, 0, 1.0f);
+        FlagManager.Instance.ResetFlags();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (FlagManager.Instance.flags[0]==true)
         {
-            cap.transform.rotation = Quaternion.Euler(45, 0, 0);
-            meshrender.material.color = new Color(0, 0, 0, 0.0f);
+            if (FlagManager.Instance.flags[1] == false)
+            {
+                cap.transform.rotation = Quaternion.Euler(45, 0, 0);
+                yogo.SetActive(true);
+                FlagManager.Instance.flags[1] = true;
+            }
+        }
+        endflag = yogo.GetComponent<gomi>().gomiflag;
+        if (endflag == false)
+        {
+            FlagManager.Instance.flags[4] = true;
+            FlagManager.Instance.flags[3] = false;
+        }
+        if (FlagManager.Instance.flags[4] == true)
+        {
+            moveing.SetActive(true);
+        }
+        if (FlagManager.Instance.flags[5] == true)
+        {
+            FlagManager.Instance.flags[3] = false;
+            FlagManager.Instance.flags[6] = true;
+        }
+        else
+        {
+            FlagManager.Instance.flags[6] = false;
+        }
+        if (FlagManager.Instance.flags[5] == false && FlagManager.Instance.flags[0] == true)
+        {
+            FlagManager.Instance.flags[3] = true;
         }
     }
 }
