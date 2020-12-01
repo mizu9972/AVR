25355cf0333a71e400e85be1b1db13be4ac7dcdf
RyuichiMasuyama <57240248+RyuichiMasuyama@users.noreply.github.com>
2020-12-01 13:19:08 +0900

    ステージ関連のプログラム管理するやつ作った、プログラムは私の意のままに

---
+++ ProjectAVR/Assets/Script/Horror/SelectScene/SelectDoor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectDoor : MonoBehaviour
+{
+    // animetion再生するよう
+    Animator animator;
+
+    // ステージenum
+    public enum Door : short {
+        Stage1,
+        Stage2,
+        Stage3,
+        Max,
+    }
+
+    // このドアはどこに繋がっているか
+    [SerializeField]
+    private Door door;
+    
+    // ステージの文字列でロードする
+    // そのために事前にリスト化
+    readonly string[] doorName = new string[(int)Door.Max] {
+        "Stage1",
+        "Stage2",
+        "Stage3"
+    };
+
+    // start-up
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // update
+    private void Update()
+    {
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        // todo:　個々の部分本舘にパスしてロードを見直す
+        //if (/*手が触れていてトリガーが引かれていたら*/)
+        //{
+        //    // animator.SetTrigger("opne");
+        //    SceneManager.LoadScene(doorName[(int)door]);  
+        //}
+    }
+
+}
---
+++ ProjectAVR/Assets/Script/Horror/Stage/Stage2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stage
+{
+    public class Stage2 : GameSystem
+    {
+        // Start is called before the first frame update
+        public void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        public void Update()
+        {
+
+        }
+    }
+}
