a846d75ad44690993ecd7232a52e943a17c7672f
jyura <life_jyura@yahoo.co.jp>
2020-12-01 19:23:50 +0900

    stage2修正

--- ProjectAVR/Assets/tatemoto/script/prologscript.cs
+++ ProjectAVR/Assets/tatemoto/script/prologscript.cs
@@ -11,44 +11,44 @@ public class prologscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FlagManager.Instance.ResetFlags();
+        //FlagManager.Instance.ResetFlags();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (FlagManager.Instance.flags[0]==true)
-        {
-            if (FlagManager.Instance.flags[1] == false)
-            {
-                cap.transform.rotation = Quaternion.Euler(45, 0, 0);
-                yogo.SetActive(true);
-                FlagManager.Instance.flags[1] = true;
-            }
-        }
-        endflag = yogo.GetComponent<gomi>().gomiflag;
-        if (endflag == false)
-        {
-            FlagManager.Instance.flags[4] = true;
-            FlagManager.Instance.flags[3] = false;
-        }
-        if (FlagManager.Instance.flags[4] == true)
-        {
-            moveing.SetActive(true);
-            FlagManager.Instance.flags[3] = false;
-        }
-        if (FlagManager.Instance.flags[5] == true)
-        {
-            FlagManager.Instance.flags[3] = false;
-            FlagManager.Instance.flags[6] = true;
-        }
-        else
-        {
-            FlagManager.Instance.flags[6] = false;
-        }
-        if (FlagManager.Instance.flags[5] == false && FlagManager.Instance.flags[0] == true)
-        {
-            FlagManager.Instance.flags[3] = true;
-        }
+        //if (FlagManager.Instance.flags[0]==true)
+        //{
+        //    if (FlagManager.Instance.flags[1] == false)
+        //    {
+        //        cap.transform.rotation = Quaternion.Euler(45, 0, 0);
+        //        yogo.SetActive(true);
+        //        FlagManager.Instance.flags[1] = true;
+        //    }
+        //}
+        //endflag = yogo.GetComponent<gomi>().gomiflag;
+        //if (endflag == false)
+        //{
+        //    FlagManager.Instance.flags[4] = true;
+        //    FlagManager.Instance.flags[3] = false;
+        //}
+        //if (FlagManager.Instance.flags[4] == true)
+        //{
+        //    moveing.SetActive(true);
+        //    FlagManager.Instance.flags[3] = false;
+        //}
+        //if (FlagManager.Instance.flags[5] == true)
+        //{
+        //    FlagManager.Instance.flags[3] = false;
+        //    FlagManager.Instance.flags[6] = true;
+        //}
+        //else
+        //{
+        //    FlagManager.Instance.flags[6] = false;
+        //}
+        //if (FlagManager.Instance.flags[5] == false && FlagManager.Instance.flags[0] == true)
+        //{
+        //    FlagManager.Instance.flags[3] = true;
+        //}
     }
 }
