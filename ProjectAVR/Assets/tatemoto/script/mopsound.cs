46e318078e6fa4e6acdf5247341a0a95394fe825
jyura <life_jyura@yahoo.co.jp>
2020-12-07 08:11:01 +0900

    stage2追加

---
+++ ProjectAVR/Assets/tatemoto/script/mopsound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class mopsound : MonoBehaviour
+{
+    public AudioClip audioClip1;
+    private AudioSource audioSource;
+    public float volume;
+    Rigidbody rb;
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip1;
+        //audioSource.volume = volume;
+        rb = this.transform.GetComponent<Rigidbody>();
+    }
+    public void OnCollisionEntery(Collider other)
+    {
+        audioSource.Play();
+        rb = this.transform.GetComponent<Rigidbody>();
+        volume = rb.velocity.magnitude;
+    }
+    void Update()
+    {
+
+    }
+}
