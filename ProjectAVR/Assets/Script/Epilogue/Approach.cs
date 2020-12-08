76dde2443a9aa2f91cbedcfd38534ba6ac3da398
syu0417 <s_kiba84@yahoo.co.jp>
2020-12-08 18:27:52 +0900

    エピローグの手の移動処理を作成。あとは順番に移動させるだけ。エンドロールシーンにキャンバス作成。

---
+++ ProjectAVR/Assets/Script/Epilogue/Approach.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Approach : MonoBehaviour
+{
+    [Header("スタート位置オブジェクト")]
+    public GameObject m_Start = null;
+
+    [Header("終了位置オブジェクト")]
+    public GameObject m_End = null;
+
+    [Header("手オブジェクト")]
+    public List<GameObject> m_Hands = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(var Hand in m_Hands)//全ての手オブジェクトにスタートとエンドをセット
+        {
+            Hand.GetComponent<MoveHand>().StartObj = m_Start;
+            Hand.GetComponent<MoveHand>().EndObj = m_End;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MoveStart(int value)//HandのActiveをtrueに
+    {
+        m_Hands[value].GetComponent<MoveHand>().isActive = true;
+    }
+
+    
+}
