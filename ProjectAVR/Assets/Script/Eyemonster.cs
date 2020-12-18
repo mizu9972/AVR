1d4c5481b69cb58288595d1c7685192152adfaa0
leo <kikoushi18@gmail.com>
2020-12-18 16:31:29 +0900

    め

---
+++ ProjectAVR/Assets/Script/Eyemonster.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyemonster : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private GameObject eye;
+    private int i = 0;
+    private bool flg = false;
+
+    int cnt = 0;
+
+    void Start()
+    {
+        eye= GameObject.Find("pTorus36");
+    }
+
+    void Update()
+    {
+        GuruGuru();
+    }
+
+    void GuruGuru()
+    {
+        if (!flg)
+        {
+            var targetPos = player.transform.position;
+            var targetRot = Quaternion.LookRotation(targetPos - eye.transform.position);
+            eye.transform.rotation = Quaternion.RotateTowards(eye.transform.rotation, targetRot, Time.deltaTime * 150);
+            RandumNum();
+        }
+        else
+        {
+            var targetPos = player.transform.position+new Vector3(Ranyeah(), Ranyeah(), Ranyeah());
+            var targetRot = Quaternion.LookRotation(targetPos - eye.transform.position);
+            eye.transform.rotation = Quaternion.RotateTowards(eye.transform.rotation, targetRot, Time.deltaTime * 360);
+
+            cnt++;
+            if (cnt>60)
+            {
+                cnt = 0;
+                flg = false;
+            }
+        }
+    }
+
+    void RandumNum()
+    {
+        i = Random.Range(0, 10);
+        if (i == 10)
+        {
+            flg = true;
+        }
+    }
+
+    int Ranyeah()
+    {
+        int j = Random.Range(-5, 6);
+        if (j <= 0)
+        {
+            j = -5;
+        }
+        else
+        {
+            j = 6;
+        }
+        return j;
+    }
+}
