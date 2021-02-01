0feaac5afe6c6aa6509a29db1c3fbfc38c370eaf
syu0417 <s_kiba84@yahoo.co.jp>
2021-02-01 21:43:40 +0900

    追加スクリプト

---
+++ ProjectAVR/Assets/Script/Epilogue/AdjustCenterPos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustCenterPos : MonoBehaviour
+{
+    [Header("計算に使用する数値")]
+    public float Num;
+
+    public GetPoint m_getPoint;
+    private float m_OldDistance;//前の距離
+    private float m_NowDistance;//
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    [ContextMenu("拡大")]
+    private void ExpantionCircle()
+    {
+        //それぞれに値使用して拡大
+    }
+
+    [ContextMenu("縮小")]
+    private void ShrinkCircle()
+    {
+        //それぞれに値使用して縮小
+    }
+}
